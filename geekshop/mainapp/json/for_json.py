46ba6c884e06d2b7f2df482ae5f5239d2ec81663
Alex <hlbu@yandex.ru>
2020-12-06 18:26:13 +0300

    hw2++++

--- geekshop/mainapp/json/for_json.py
+++ geekshop/mainapp/json/for_json.py
@@ -1,7 +1,6 @@
 import json
 from pprint import pprint
 
-
 data = {'title': 'weelcom',
         'username': 'ALex',
         'goods': [
@@ -14,18 +13,14 @@ data = {'title': 'weelcom',
         ],
         }
 
-with open('m_cadrs.json', 'w') as f:
+with open('m_cadrs.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=4)
 
-# with open('mus_cadrs.json', 'r', encoding='utf-8') as f:
-#     cards = json.load(f)
-#
-# for el in cards:
-#     # print(el['categ'])
-#     # print(el['goods'])
-#     print(el['goods'][2]['name'])
-#     print(el['goods'][2]['cost'])
-#     print(el['goods'][2]['description'])
-#     print(el['goods'][2]['quantity'])
-#     print(el['goods'][2]['image'])
-#     print('================================')
+with open('m_cadrs.json', 'r', encoding='utf-8') as f:
+    cards = json.load(f)
+# #
+for el in cards:
+    print(el)
+print(data['goods'])
+for qwe in data['goods']:
+    print(qwe['name'], qwe['price'])
