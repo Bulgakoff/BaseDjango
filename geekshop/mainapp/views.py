6e7035579d9a3bd995034c1dda5f2e6fcf7b096d
Alex <hlbu@yandex.ru>
2020-12-10 15:55:24 +0300

    правки по репе idea не удаляется никак

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-import json
 from mainapp.models import ProductCategory, Products
 
-JSON_PATH = 'mainapp/json/'
-
-
-def json_from(file_json):
-    with open(JSON_PATH + file_json, 'r', encoding='utf-8') as f:
-        return json.load(f)
 
 
 def index(request):
@@ -18,29 +11,15 @@ def index(request):
 
 
 def products(request):
-    json_products = json_from('cloth_cadrs.json')
     products_db = Products.objects.all()
     categories_db = ProductCategory.objects.all()
 
     context = {
         'title': 'продукты',
-        # 'prs': json_products,
         'prds_db': products_db,
         'categs_db': categories_db,
     }
     return render(request, 'mainapp/products.html', context)
 
 
-def contex(request):
-    context = json_from('m_cadrs.json')
 
-    return render(request, 'mainapp/contex.html', context)
-
-
-def new(request):
-    ppp = Products.objects.all()
-    context = {
-        'title': 'главная',
-        'prds': ppp,
-    }
-    return render(request, 'mainapp/new.html', context)
