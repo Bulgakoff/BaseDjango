3aed30dce17d7978d1fbcb7ebb5ff85250cef3ce
Alex <hlbu@yandex.ru>
2021-01-14 12:46:38 +0300

    geekshop/+++++++

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -8,12 +8,17 @@ from django.utils.timezone import now
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveSmallIntegerField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
 
-    activation_key = models.CharField(max_length=128,blank=True, null=True)
-    activation_key_expires = models.DateTimeField(default=(now()+timedelta(hours=48)))
+    activation_key = models.CharField(max_length=128, blank=True, null=True)
+    activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def is_activation_key_expired(self):
-        if now()< self.activation_key_expires:
+        if now() < self.activation_key_expires:
             return False
         return True
 
