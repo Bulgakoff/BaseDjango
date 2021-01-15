0721848554361a249d8d53e4f8faa14b268af5cd
Alex <hlbu@yandex.ru>
2021-01-15 16:57:06 +0300

    geekshop/+++1++

--- geekshop/authapp/context_processors.py
+++ geekshop/authapp/context_processors.py
@@ -1,12 +1,12 @@
 from basketapp.models import Basket
 
+
 def basket(request):
-   print(f'context processor basket works')
-   basket_item = []
+    print(f'context processor basket works')
+    basket_item = []
 
-   if request.user.is_authenticated:
-       basket_item = Basket.objects.filter(user=request.user)
-
-   return {
-       'baskets': basket_item,
-   }
+    if request.user.is_authenticated:
+        basket_item = Basket.objects.filter(user=request.user)
+    return {
+        'baskets': basket_item,
+    }
--- geekshop/blockchain/bch.py
+++
@@ -1,4 +0,0 @@
-import hashlib
-
-block = hashlib.sha1(b'Fkred').hexdigest()
-print(block)
