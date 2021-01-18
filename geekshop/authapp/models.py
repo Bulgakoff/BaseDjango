2276d515b525d0f3869534d48a1c8f7ea5c2d969
Alex <hlbu@yandex.ru>
2021-01-18 11:54:23 +0300

    geekshop/dd2_1+

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -7,11 +7,10 @@ from django.utils.timezone import now
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveSmallIntegerField(verbose_name='возраст',  default=18)
+    age = models.PositiveSmallIntegerField(verbose_name='возраст', default=18)
 
     activation_key = models.CharField(max_length=128, blank=True, null=True)
     activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
-
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -24,3 +23,18 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    tagline = models.CharField(verbose_name='теги', max_length=128, blank=True)
+    aboutMe = models.TextField(verbose_name='о себе', max_length=512, blank=True)
+    gender = models.CharField(verbose_name='пол', max_length=1, choices=GENDER_CHOICES, blank=True)
