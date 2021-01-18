a29d82c1c6fbca5041954ab9a48d200cf412754b
Alex <hlbu@yandex.ru>
2021-01-18 19:54:07 +0300

    geekshop/dd2_1++++fin+

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -63,7 +63,7 @@ class UserProfileForm(UserChangeForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'avatar', 'username', 'email',)
+        fields = ('first_name', 'last_name', 'avatar', 'username', 'email','age')
 
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
