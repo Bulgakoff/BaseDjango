ac7565344503587c996094de9d226f25799a72d6
Alex <hlbu@yandex.ru>
2021-02-03 14:38:30 +0300

    geekshop/3+++++++

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -19,8 +19,8 @@ class Basket(models.Model):
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
     def get_items_basket_cached(self):
-        # return Basket.objects.filter(user=self.user)
-        return self.user.basket_set.select_related()
+        return Basket.objects.filter(user=self.user)
+        # return self.user.basket_set.select_related()
 
     def __str__(self):
         return f'Корзина для {self.user.username}|Продукт  для {self.product.name}'
@@ -29,11 +29,13 @@ class Basket(models.Model):
         return self.quantity * self.product.price
 
     def total_qu(self):
+        # baskets = Basket.objects.filter(user=self.user)
         baskets = self.get_items_basket_cached()
         return sum(basket.quantity for basket in baskets)
 
     def total_summa(self):
-        baskets = Basket.objects.filter(user=self.user)
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_basket_cached()
         return sum(basket.summa_product() for basket in baskets)
 
     # def delete(self):
