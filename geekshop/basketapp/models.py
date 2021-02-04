c8e85384b092a5ea6fd2298b187ce3e8ab6bddc5
Alex <hlbu@yandex.ru>
2021-02-05 00:06:20 +0300

    geekshop/4+++++++++++++++++

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -20,7 +20,7 @@ class Basket(models.Model):
     quantity = models.PositiveSmallIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
-    # @cached_property
+    @cached_property
     # @property
     def get_items_basket_cached(self):
         return Basket.objects.filter(user=self.user).select_related()
@@ -33,14 +33,14 @@ class Basket(models.Model):
         return self.quantity * self.product.price
 
     def total_qu(self):
-        baskets = Basket.objects.filter(user=self.user)
-        # baskets = self.get_items_basket_cached  # если используется @cached_property  стр.23 кэшировани
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_basket_cached  # если используется @cached_property  стр.23 кэшировани
         # е то обращение происходит не как к методу а как к свойству
         return sum(basket.quantity for basket in baskets)
 
     def total_summa(self):
-        baskets = Basket.objects.filter(user=self.user)
-        # baskets = self.get_items_basket_cached  # если используется @cached_property кэширование
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_basket_cached  # если используется @cached_property кэширование
         # то обращение происходит не как к методу а как к свойству
         return sum(basket.summa_product() for basket in baskets)
 
